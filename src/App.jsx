3b4b4fb754ab0f0b916ff7e24e7f2d43f1bf0b60
Alex <baklan4ik1993@gmail.com>
2023-08-01 23:55:00 +0200

    new cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
 import Experience from './components/Experience'
+import Cursor from './helpers/Cursor'
 function App() {
   return (
     <div className="app h-[100vh] w-[100%]">
-      
       <Experience />
+      <Cursor />
     </div>
   )
 }
