3edca09f7667b41d553cf12da9371228df95930b
Alex <baklan4ik1993@gmail.com>
2023-08-03 14:49:37 +0200

    scalling html down

--- src/components/HtmlPart.jsx
+++ src/components/HtmlPart.jsx
@@ -13,7 +13,7 @@ const HtmlPart = ({isOpen, setIsOpen}) => {
 
     <motion.div   animate={isOpen ? "open"  : "closed"}
     variants={variants}>
-        <motion.div variants={parentAnimation} initial="hidden" animate="show" className="  p-3 sm:p-7 md:p-10 absolute flex flex-col gap-3 sm:gap-5 md:gap-7 lg:gap-10 xl:w-[35rem] md:w-[30rem] sm:w-[20rem] w-[10rem] 2xl:text-9xl lg:text-8xl md:text-7xl sm:text-6xl text-2xl">
+        <motion.div variants={parentAnimation} initial="hidden" animate="show" className="  p-2 sm:p-6 md:p-8 absolute flex flex-col gap-2 sm:gap-4 md:gap-6 lg:gap-8 xl:w-[25rem] md:w-[20rem] sm:w-[15rem] w-[8rem] 2xl:text-7xl lg:text-6xl md:text-5xl sm:text-4xl text-xl">
             <div  className="flex w-full justify-between">
             <div className="overflow-hidden"><motion.h1 variants={firstnameAnimation}   className="title " >A</motion.h1></div>
             <div className="overflow-hidden"><motion.h1 variants={firstnameAnimation}   className="title " >L</motion.h1></div>
@@ -24,21 +24,21 @@ const HtmlPart = ({isOpen, setIsOpen}) => {
             </div>
         
         </motion.div>
-        <motion.div variants={simpleAnimation} initial="hidden" animate="show"  className="explore absolute top-0 right-0  p-3 sm:p-7 md:p-10 w-fit flex flex-col  gap-2 sm:text-lg text-xs">
-            <div className="overflow-hidden flex justify-between gap-3 items-end"><h3 className="name text-stone-950">Get in Touch!</h3><div className="line w-20 h-[3px]  bg-[#3e3e3d] " /><h3 className="name text-right">I'm a</h3></div>
+        <motion.div variants={simpleAnimation} initial="hidden" animate="show"  className="explore absolute top-0 right-0  p-2 sm:p-6 md:p-8 w-fit flex flex-col  gap-2 sm:text-base text-xs">
+            <div className="overflow-hidden flex justify-between gap-1 items-end"><h3 className="name text-stone-950">Get in Touch!</h3><div className="line w-12 lg:w-16 h-[3px]  bg-[#3e3e3d] " /><h3 className="name text-right">I'm a</h3></div>
             <div className="overflow-hidden text-right"><h3 className='name '>software developer</h3></div>
             <div className="line w-full h-[3px]  bg-[#3e3e3d]" />
         </motion.div>
 
-        <motion.div variants={simpleAnimation} initial="hidden" animate="show" className="absolute left-[10%] bottom-48 w-96 ">
-            <p className="md:text-lg lg:text-xl text-sm mb-8">Passionate developer proficient in <span className="text-teal-500">Django</span>, <span className="text-cyan-500">React.js</span>, <span className="text-stone-950">Next.js</span>, and <span className="text-stone-950">Three.js</span>.</p>
+        <motion.div variants={simpleAnimation} initial="hidden" animate="show" className="absolute left-0  bottom-0 p-20 w-fit ">
+            <p className="md:text-base lg:text-lg text-xs mb-6">Passionate developer proficient <br/> in <span className="text-teal-500">Django</span>, <span className="text-cyan-500">React.js</span>, <span className="text-stone-950">Next.js</span>, and <span className="text-stone-950">Three.js</span>.</p>
             <div className="explore w-fit">
-                <motion.h3 onClick={()=>{setIsOpen(false)}}  className="text-base md:text-xl lg:text-2xl w-fit py-3">Find Out More & Get in Touch</motion.h3>
+                <motion.h3 onClick={()=>{setIsOpen(false)}}  className="text-sm md:text-lg lg:text-xl w-fit py-2">Find Out More & Get in Touch</motion.h3>
                 <div className="line w-full h-[3px]  bg-[#3e3e3d]" />
             </div>
         </motion.div>
 
-        <motion.div variants={parentAnimation} initial="hidden" animate="show" className=" p-3 sm:p-7 md:p-10 absolute right-0 bottom-0  flex flex-col gap-3 sm:gap-5 md:gap-7 lg:gap-10 2xl:text-9xl lg:text-8xl md:text-7xl sm:text-6xl text-2xl">
+        <motion.div variants={parentAnimation} initial="hidden" animate="show" className=" p-2 sm:p-6 md:p-8 absolute right-0 bottom-0  flex flex-col gap-2 sm:gap-4 md:gap-6 lg:gap-8 2xl:text-7xl lg:text-6xl md:text-5xl sm:text-4xl text-xl">
             <div className="flex w-full">
                <div className="overflow-hidden"> <motion.h1 variants={firstnameAnimation}  className="title " >B</motion.h1></div>
                <div className="overflow-hidden"> <motion.h1 variants={firstnameAnimation}  className="title " >U</motion.h1></div>
